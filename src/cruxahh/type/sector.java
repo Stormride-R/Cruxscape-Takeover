214ff2fbc8999fba09d189809901caf6379c5e02
Stormrider <154956687+Stormride-R@users.noreply.github.com>
2024-08-18 14:20:48 +0800

    Update sector.java

--- src/cruxahh/type/sector.java
+++ src/cruxahh/type/sector.java
@@ -18,16 +18,15 @@ import mindustry.ui.*;
 import mindustry.world.modules.*;
 
 import static mindustry.Vars.*;
-public float threat;
 
 public class sector{
-	
+	public float threat;
 	public String displayThreat(){
-    float step = 0.125f;
-    String color = Tmp.c1.set(Color.white).lerp(Color.scarlet, Mathf.round(threat, step)).toString();
-    String[] threats = {"class I", "class II", "class III", "class IV", "class V", "class VI", "class VII", "class VIII", "class IX"};
-    int index = Math.min((int)(threat / step), threats.length - 1);
-    return "[#" + color + "]" + Core.bundle.get("threat." + threats[index]);
-    }
+	float step = 0.125f;
+	String color = Tmp.c1.set(Color.white).lerp(Color.scarlet, Mathf.round(threat, step)).toString();
+	String[] threats = {"class I", "class II", "class III", "class IV", "class V", "class VI", "class VII", "class VIII", "class IX"};
+	int index = Math.min((int)(threat / step), threats.length - 1);
+	return "[#" + color + "]" + Core.bundle.get("threat." + threats[index]);
+	}
 	
 }
