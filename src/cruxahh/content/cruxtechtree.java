973fb8613154c95db9cf4a36d2b490df1b4ec5c7
Stormrider <154956687+Stormride-R@users.noreply.github.com>
2024-08-19 16:52:12 +0800

    Update cruxtechtree.java

--- src/cruxahh/content/cruxtechtree.java
+++ src/cruxahh/content/cruxtechtree.java
@@ -1,7 +1,5 @@
 package cruxahh.content;
 
-import mindustry.game.Objectives.*;
-
 import arc.struct.Seq;
 import mindustry.content.Blocks;
 import mindustry.content.SectorPresets;
@@ -11,49 +9,26 @@ import mindustry.ctype.UnlockableContent;
 import mindustry.game.Objectives;
 import mindustry.type.ItemStack;
 import mindustry.content.TechTree;
-
 import static mindustry.content.TechTree.*;
-
 public class cruxtechtree {
     public static TechNode context;
 
     public static void load() {
+	margeNode(groundZero, () -> {
 	margeNode(SectorPresets.groundZero, () -> {
-            node(sectorset.frigidTundra,Seq.with(new SectorComplete(groundZero),new Research(copperWallLarge),new Research(underflowGate)));
+            node(sectorset.frigidTundra);
 	});
     }
-
-
     private static void margeNode(UnlockableContent parent, Runnable children) {
         context = TechTree.all.find(t -> t.content == parent);
         children.run();
     }
-
-
     private static void node(UnlockableContent content, ItemStack[] requirements, Seq<Objectives.Objective> objectives, Runnable children) {
         TechNode node = new TechNode(context, content, requirements);
         if (objectives != null) node.objectives = objectives;
-
-
         TechNode prev = context;
         context = node;
         children.run();
         context = prev;
     }
 
-
-    private static void node(UnlockableContent content, ItemStack[] requirements, Runnable children) {
-        node(content, requirements, null, children);
-    }
-
-
-    private static void node(UnlockableContent content, Runnable children) {
-        node(content, content.researchRequirements(), children);
-    }
-
-
-    private static void node(UnlockableContent block) {
-        node(block, () -> {
-        });
-    }
-}
