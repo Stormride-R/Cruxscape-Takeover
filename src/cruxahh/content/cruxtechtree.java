6d3e87db97cdf3265bfcf1eb60dc77ee120339ae
Stormrider <154956687+Stormride-R@users.noreply.github.com>
2024-08-27 22:59:34 +0800

    Add files via upload

--- src/cruxahh/content/cruxtechtree.java
+++ src/cruxahh/content/cruxtechtree.java
@@ -1,51 +1,31 @@
 package cruxahh.content;
 
-import arc.struct.Seq;
-import mindustry.content.Blocks;
-import mindustry.content.SectorPresets;
-import mindustry.content.Items;
-import mindustry.content.UnitTypes;
-import mindustry.ctype.UnlockableContent;
-import mindustry.game.Objectives;
-import mindustry.type.ItemStack;
-import mindustry.content.TechTree;
 
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+import arc.struct.*;
+import mindustry.content.Planets;
+import mindustry.content.TechTree;
+import mindustry.game.Objectives.*;
+import mindustry.type.ItemStack;
+import cruxahh.content.*;
+import cruxahh.content.blocks.*;
+import static mindustry.content.Blocks.*;
 import static mindustry.content.TechTree.*;
+import static mindustry.content.UnitTypes.*;
+import static mindustry.content.SectorPresets.*;
 
 public class cruxtechtree {
-    public static TechNode context;
-
-    public static void load() {
-	margeNode(SectorPresets.groundZero, () -> {
-            node(sectorset.frigidTundra);
-	});
-    }
-
-    private static void margeNode(UnlockableContent parent, Runnable children) {
-        context = TechTree.all.find(t -> t.content == parent);
-        children.run();
-    }
-
-    private static void node(UnlockableContent content, ItemStack[] requirements, Seq<Objectives.Objective> objectives, Runnable children) {
-        TechNode node = new TechNode(context, content, requirements);
-        if (objectives != null) node.objectives = objectives;
-
-        TechNode prev = context;
-        context = node;
-        children.run();
-        context = prev;
-    }
-
-    private static void node(UnlockableContent content, ItemStack[] requirements, Runnable children) {
-        node(content, requirements, null, children);
-    }
-
-    private static void node(UnlockableContent content, Runnable children) {
-        node(content, content.researchRequirements(), children);
-    }
-
-    private static void node(UnlockableContent block) {
-        node(block, () -> {
-        });
-    }
+    public static void load(){
+        nodeRoot("Cruxscape-serpulo", coreShard, false, () -> {
+            //Above the skies is the currently the placeholder for now, the required sector will changes later.
+            node(sectorset.frigidTundra, Seq.with(new SectorComplete(groundZero)),)
+		})
+    };
 }
