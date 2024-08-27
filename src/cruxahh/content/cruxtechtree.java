93daeb488354fd9e66aa4b647f73bd542e362320
Stormrider <154956687+Stormride-R@users.noreply.github.com>
2024-08-27 23:20:17 +0800

    Update cruxtechtree.java

--- src/cruxahh/content/cruxtechtree.java
+++ src/cruxahh/content/cruxtechtree.java
@@ -15,6 +15,7 @@ import mindustry.content.TechTree;
 import mindustry.game.Objectives.*;
 import mindustry.type.ItemStack;
 import cruxahh.content.*;
+import cruxahh.content.sectorset.*;
 import static mindustry.content.Blocks.*;
 import static mindustry.content.TechTree.*;
 import static mindustry.content.UnitTypes.*;
@@ -23,7 +24,6 @@ import static mindustry.content.SectorPresets.*;
 public class cruxtechtree {
     public static void load(){
         nodeRoot("Cruxscape-serpulo", coreShard, false, () -> {
-            //Above the skies is the currently the placeholder for now, the required sector will changes later.
             node(sectorset.frigidTundra, Seq.with(
             new SectorComplete(groundZero)
 	    ), () -> {
